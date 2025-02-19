30fd6fd8c8f5e06e3e2500b65402c05da9621c9a
Soufiane Ar Razouki <soufiane.arrazouki@clearchannel.be>
2025-02-19 08:18:59 +0100

    /

---
+++ src/main_tool.py
@@ -0,0 +1,4 @@
+import serial
+print("Hello from jupitux")
+print("this is just a test in order to determine if deb file is the best approach in this use case.")
+print("I'll install update apt packages.")
